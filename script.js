a7c34b68360107c162f306803996f72d9c227a71
aliciadenk-afk <alicia.denk@escola.pr.gov.br>
2026-08-20 08:49:24 -0300

    ok

--- script.js
+++ script.js
@@ -6,7 +6,33 @@ document.addEventListener("DOMcontentLoaded", () =>{
 
     //localiza o contador 
 
-    let textNode = Array
+    let textNode = Array.from(likeBtn.childNodes).find(node)=> node.nodeType
+    === Node. TEXT_NODE && node.textContent.trim() !==""
+);
+
+//zera o contador 
+
+let cont = 0 
+
+//atualiza
+
+if(textNode){
+    textNode.textContent = `0`;
+}
+
+//coração]
+function applyLikedStyle (){
+ likeSvg.style.fill ="#eeafd9"
+ likeSvg.style.stroke ="#ff69cd"
+ likeSvg.style.color ="#ace3ec"
+ 
+
+ //efeito curtida
+ likeSvg.style.transform = "scale(1.3";
+ setTimeout(() => (likeSvg.style.transform = "scale(1)"))
+}
 
 
-    })
+})
+
+
